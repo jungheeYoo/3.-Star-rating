1cea0e9a2497ea6ed6d98e18b13070e1c815d6fd
Joy <jungheeyoo.joy@gmail.com>
2024-05-10 23:37:11 +0900

    별점 컨테이너 생성 완료

--- star-rating/index.js
+++ star-rating/index.js
@@ -1,3 +1,41 @@
 // do something!
 
-function StarRating() {}
+const CONTAINER_CLASS_NAME = 'star-rating-container';
+const cssStyle = './star-rating/theme.css';
+
+// <link href="star-rating/theme.css" rel="stylesheet" />
+const loadStyle = (href) => {
+  if (document.querySelector(`link[href="${href}"]`)) return;
+
+  const link = document.createElement('link');
+  link.href = href;
+  link.rel = 'stylesheet';
+
+  const lastLink = document.querySelector('link:last-of-type');
+
+  document.head.insertBefore(link, lastLink.nextElementSibling);
+};
+
+const makeStarRating = (maxRating) => {
+  const StarRatingCon = document.createElement('div');
+  StarRatingCon.className = CONTAINER_CLASS_NAME;
+
+  for (let i = 0; i < maxRating; i++) {
+    const star = document.createElement('i');
+    star.dataset.id = i + 1;
+    star.className = 'bx bxs-star';
+    StarRatingCon.appendChild(star);
+  }
+  return StarRatingCon;
+};
+
+const StarRating = ($container) => {
+  loadStyle(cssStyle);
+
+  const maxRating = $container.dataset.maxRating;
+
+  const StarRatingCon = makeStarRating(maxRating);
+  $container.appendChild(StarRatingCon);
+};
+
+export default StarRating;
