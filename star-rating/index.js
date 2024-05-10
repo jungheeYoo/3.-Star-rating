fd0360085c8f61e6a6971f8790f52757788dda91
Joy <jungheeyoo.joy@gmail.com>
2024-05-11 01:42:26 +0900

    마우스 오버 이벤트 완료

--- star-rating/index.js
+++ star-rating/index.js
@@ -1,9 +1,10 @@
 // do something!
 
 const CONTAINER_CLASS_NAME = 'star-rating-container';
-const cssStyle = './star-rating/theme.css';
+const STAR_RATING_STYLE = './star-rating/theme.css';
 
 // <link href="star-rating/theme.css" rel="stylesheet" />
+// star-rating-container CSS 동적으로 추가
 const loadStyle = (href) => {
   if (document.querySelector(`link[href="${href}"]`)) return;
 
@@ -16,9 +17,12 @@ const loadStyle = (href) => {
   document.head.insertBefore(link, lastLink.nextElementSibling);
 };
 
-const makeStarRating = (maxRating) => {
+// 별점 생성
+const makeStarRating = ($container) => {
+  const maxRating = $container.dataset.maxRating;
   const StarRatingCon = document.createElement('div');
   StarRatingCon.className = CONTAINER_CLASS_NAME;
+  $container.appendChild(StarRatingCon);
 
   for (let i = 0; i < maxRating; i++) {
     const star = document.createElement('i');
@@ -29,13 +33,28 @@ const makeStarRating = (maxRating) => {
   return StarRatingCon;
 };
 
+// 마우스 오버 이벤트
+const handleStarRating = ($container) => {
+  const stars = [...$container.querySelectorAll('i')];
+
+  $container.addEventListener('mouseover', (event) => {
+    const id = event.target.dataset.id;
+    stars.forEach((star, i) => {
+      star.classList.toggle('hovered', i < id);
+    });
+  });
+
+  $container.addEventListener('mouseout', () => {
+    stars.forEach((star, i) => {
+      star.classList.remove('hovered');
+    });
+  });
+};
+
 const StarRating = ($container) => {
-  loadStyle(cssStyle);
-
-  const maxRating = $container.dataset.maxRating;
-
-  const StarRatingCon = makeStarRating(maxRating);
-  $container.appendChild(StarRatingCon);
+  loadStyle(STAR_RATING_STYLE);
+  makeStarRating($container);
+  handleStarRating($container);
 };
 
 export default StarRating;
